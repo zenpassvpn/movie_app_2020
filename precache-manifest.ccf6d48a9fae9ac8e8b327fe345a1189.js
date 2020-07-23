59bce8795d48e4fce229429ad9b4bdf9711f5ec2
develop.transdata <develop.transdata@gmail.com>
2020-07-23 16:33:27 +0900

    Updates

---
+++ precache-manifest.ccf6d48a9fae9ac8e8b327fe345a1189.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee334ead5c9140996e11ee39a52e1561",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "d38228906dcc165742ca",
+    "url": "/movie_app_2020/static/js/2.1fce4192.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.1fce4192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf525f6b1766dff3c8c6",
+    "url": "/movie_app_2020/static/js/main.e8bf75b4.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
